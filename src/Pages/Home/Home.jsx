42f9b60b930562f9ef123ed654ade4d19e55fa71
Ahammad204 <Ahammadcode@gmail.com>
2023-11-04 23:20:55 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <div>Hello world!</div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Router/BoigorRouter.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+      children:[
+
+        {
+
+            path:'/',
+            element: <Home></Home>
+
+        }
+
+
+      ]
+    },
+  ]);
+
+export default router;
