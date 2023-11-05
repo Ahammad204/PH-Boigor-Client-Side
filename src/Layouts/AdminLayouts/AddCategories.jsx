9e81330ba0e894d2fe4f6e02d95baf69d36216eb
Ahammad204 <Ahammadcode@gmail.com>
2023-11-05 17:50:34 +0600

    Setup Firebase

---
+++ src/Layouts/AdminLayouts/AddCategories.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCategories = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddCategories;
---
+++ src/Pages/Home/BookCategory/BookCategories.jsx
@@ -0,0 +1,11 @@
+
+
+const BookCategories = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BookCategories;
